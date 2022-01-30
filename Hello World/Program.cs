94be27ba1c5bce70ed4af988d2b06db95194c74f
Michael Blaydes <mcblaydesconsulting@gmail.com>
2022-01-30 17:57:34 -0500

    Change to Alphabet Program

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
-namespace HelloWorld
+namespace Alphabet
 {
     class program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Alphabet = "";
+            for (char alpha = 'A'; alpha <= 'Z'; alpha++)
+            {
+                Alphabet += alpha.ToString();
+            }
+            Console.WriteLine(Alphabet);
+
+            string AlphabetZA = "";
+            for (char alpha = 'Z'; alpha >= +'A'; alpha--)
+            {
+                AlphabetZA += alpha.ToString();
+            }
+            Console.WriteLine(AlphabetZA);
+
+            string AlphabetEveryother = "";
+            for (char alpha = 'A'; alpha <= 'Z'; alpha = (char)(alpha + 2))
+            {
+                AlphabetEveryother += alpha.ToString();
+            }
+            Console.WriteLine(AlphabetEveryother);
         }
     }
 }
